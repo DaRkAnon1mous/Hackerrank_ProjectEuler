de8cb5e2e52c6650a06db921b07a57187de662c3
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2023-12-18 22:10:05 +0530

    Update Code.py

--- Project Euler #1: Multiples of 3 and 5/Code.py
+++ Project Euler #1: Multiples of 3 and 5/Code.py
@@ -1,11 +1,19 @@
+#!/bin/python3
+
 t = int(input())
 n = []
+
 for _ in range(t):
     n.append(int(input()))
+
 for num in n:
-    sum_of_multiples_of_3 = sum(i for i in range(3, num, 3))
-    sum_of_multiples_of_5 = sum(i for i in range(5, num, 5))
-    sum_of_multiples_of_15 = sum(i for i in range(15, num, 15))
+    num -= 1  # Adjust num to exclude itself from the sum if it's a multiple of 3 or 5
+
+    sum_of_multiples_of_3 = (3 * (num // 3) * (num // 3 + 1)) // 2
+    sum_of_multiples_of_5 = (5 * (num // 5) * (num // 5 + 1)) // 2
+    sum_of_multiples_of_15 = (15 * (num // 15) * (num // 15 + 1)) // 2
+
     sum_of_multiples_of_3_or_5 = sum_of_multiples_of_3 + sum_of_multiples_of_5 - sum_of_multiples_of_15
     print(sum_of_multiples_of_3_or_5)
+
 
