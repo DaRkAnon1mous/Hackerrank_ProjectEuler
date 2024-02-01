4129413a8e308d2c07580960cf2c1ae7de3957d0
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-02-01 21:12:33 +0530

    Create Code.py

---
+++ Project Euler #18: Maximum path sum I/Code.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T = int(input())
+for testcase in range(T):
+    triangle = []
+    N = int(input())
+    for i in range(N):
+        triangle.append(list(map(int, input().split())))
+
+    for row in range(N - 1, 0, -1):
+        for column in range(row):
+            triangle[row - 1][column] += max(
+                triangle[row][column], triangle[row][column + 1]
+            )
+    print(triangle[0][0])
