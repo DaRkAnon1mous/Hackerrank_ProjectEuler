2334f1ed73b1ef509cc8b01ad29febe41bc45ca6
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-02-02 13:31:31 +0530

    Create Code.py

---
+++ Project Euler #19: Counting Sundays/Code.py
@@ -0,0 +1,31 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import datetime
+
+T = int(input())
+for test_case in range(T):
+    Y_1, M_1, D_1 = map(int, input().split())
+    Y_2, M_2, D_2 = map(int, input().split())
+
+    Y_difference = Y_2 - Y_1
+    Y_1 %= 400
+    Y_1 += 400
+    Y_2 = Y_1 + Y_difference
+
+    first_of_the_month_sunday_count = 0
+
+    if D_1 == 1:
+        date = datetime.date(Y_1, M_1, 1)
+    elif M_1 == 12:
+        date = datetime.date(Y_1 + 1, 1, 1)
+    else:
+        date = datetime.date(Y_1, M_1 + 1, 1)
+
+    while date <= datetime.date(Y_2, M_2, D_2):
+        if date.isoweekday() == 7:
+            first_of_the_month_sunday_count += 1
+        if date.month == 12:
+            date = date.replace(year = date.year + 1, month = 1)
+        else:
+            date = date.replace(month = date.month + 1)
+
+    print(first_of_the_month_sunday_count)
